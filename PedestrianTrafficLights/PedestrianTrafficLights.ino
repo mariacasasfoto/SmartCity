cacf5b3c78358e4bc50578ed24e15197703609e2
dianagamedi <da.gamez.diaz@gmail.com>
2020-01-20 17:34:37 +0100

    Pedestrian traffic lights with diagram

---
+++ PedestrianTrafficLights/PedestrianTrafficLights.ino
@@ -0,0 +1,41 @@
+int redLed = 10;
+int yellowLed = 9;
+int greenLed = 8;
+int pedestrianButton = 12;
+
+void setup(){
+    pinMode(redLed, OUTPUT);
+    pinMode(yellowLed, OUTPUT);
+    pinMode(greenLed, OUTPUT);
+
+    pinMode(pedestrianButton, INPUT);
+    digitalWrite(greenLed, HIGH);
+}
+
+void loop(){
+  if (digitalRead(button) == HIGH){
+        delay(15);
+        if (digitalRead(button) == HIGH) {
+            trafficLights();
+            delay(15000); 
+        }
+    }
+}
+
+void trafficLights(){
+    digitalWrite(greenLed, LOW);
+    digitalWrite(yellowLed, HIGH);
+    delay(3000);
+
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(red, HIGH);
+    delay(5000);
+
+    digitalWrite(yellowLed, HIGH);
+    delay(2000);
+
+    digitalWrite(yellowLed, LOW);
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    delay(3000);
+}
---
+++ PedestrianTrafficLights/PedestrianTrafficLights.md
@@ -0,0 +1,10 @@
+# Pedestrian traffic lights
+What we need:
+* 1 red led
+* 1 yellow led
+* 1 green led
+* 3 100 Ohms resistor
+* 1 Push button
+* 1 10k-Ohm resistor
+
+![Diagram](Diagram.png)
